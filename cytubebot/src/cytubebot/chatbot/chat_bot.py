359a5187025ab4399be6c02444788ce5efd4eee2
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2025-09-28 14:58:24 +0100

    Adding temp debug log to video_queue

--- cytubebot/src/cytubebot/chatbot/chat_bot.py
+++ cytubebot/src/cytubebot/chatbot/chat_bot.py
@@ -39,6 +39,7 @@ class ChatBot:
         logger.info("Chatbot ready to process new videos.")
         while True:
             resp = self._db.read_stream("stream:jobs:results")
+            logger.debug("video_queue thread processing %s", resp)
             if not resp:
                 continue
 
