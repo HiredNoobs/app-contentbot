0fcc4a0de9497b63d0347b8e7c385683dd0887fe
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2026-02-19 19:45:34 +0000

    Improve configuration class

--- src/contentbot/configuration.py
+++ src/contentbot/configuration.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Any, Dict
 
 import yaml
@@ -19,19 +20,25 @@ class Configuration:
         with open(self._path) as file:
             config: Dict[str, Any] = yaml.safe_load(file)
 
+        domain = os.getenv("DOMAIN", ".com")
+
         self.cytube_url = config["cytube_url"]
         self.cytube_channel = config["cytube_channel"]
 
-        self.db_host = config["db_host"]
+        db_host = config["db_host"]
+        self.db_host = f"{db_host}.{domain}"
         self.db_port = config["db_port"]
         self.db_index = config["db_index"]
         self.db_user = config["db_user"]
-        self.db_pass = config["db_pass"]
+
         self.db_ca_cert = config["db_ca_cert"]
         self.db_cert = config["db_cert"]
         self.db_key = config["db_key"]
 
-        self.kafka_bootstrap_servers = config["kafka_bootstrap_servers"]
+        kafka_port = config["kafka_port"]
+        kafka_bootstrap_servers = config["kafka_bootstrap_servers"].split(",")
+        self.kafka_bootstrap_servers = [f"{server}.{domain}:{kafka_port}" for server in kafka_bootstrap_servers]
+
         self.kafka_content_topic = config["kafka_queue_topic"]
         self.kafka_job_topic = config["kafka_job_queue_topic"]
         self.kafka_consumer_group = config["kafka_consumer_group"]
@@ -44,6 +51,8 @@ class Configuration:
 
             self.cytube_user = secrets["cytube_user"]
             self.cytube_pass = secrets["cytube_pass"]
+
+            self.db_pass = config["db_pass"]
 
     def to_dict(self) -> dict:
         return {key: value for key, value in self.__dict__.items() if not key.startswith("_")}
